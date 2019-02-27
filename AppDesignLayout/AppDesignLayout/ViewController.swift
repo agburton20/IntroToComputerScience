eb5c18ead7c3d084cd8534d2e553a15b31a7c821
Adelaide Burton20 <agburton20@AIR-WW5RA.local>
2019-02-27 14:48:25 -0600

    AppDesignLayout

---
+++ AppDesignLayout/AppDesignLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppDesignLayout
+//
+//  Created by Adelaide Burton20 on 2/27/19.
+//  Copyright © 2019 IntroCompSci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
